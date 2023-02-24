f8c654b22f6d18cfa43716db4aa449b639e83e56
ARAOLUWA22223085 <114756077+ARAOLUWA22223085@users.noreply.github.com>
2023-02-24 17:33:11 +0000

    Added empty methods and attributes

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -10,5 +10,36 @@ namespace ConsoleAppProject.App01
     /// </author>
     public class DistanceConverter
     {
+
+        private double Miles;
+        private double Feet;
+
+        public void Run()
+        {
+        
+        }
+        /// <Summary>
+        /// Prompts user for Input value in Miles
+        /// Input value for Miles as a double number
+        ///</Summary>
+        private void InputMiles()
+        {
+
+        }
+        /// <Summary>
+        /// Applies multiplier for conversion of Miles to Feet
+        ///</Summary>
+        private void CalculateFeet()
+            {
+
+            }
+        /// <Summary>
+        /// Outputs the converted value now in Feet
+        ///</Summary>
+        private void OutputFeet()
+            {
+
+            }
+        
     }
 }
